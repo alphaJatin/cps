e61a879086146132b323af2fa8889342ca932cf2
Avii <avii.official00@gmail.com>
2021-09-06 18:35:05 +0530

    Navbar Responsiveness Added

--- Components/Footer/Footer.php
+++ Components/Footer/Footer.php
@@ -35,62 +35,79 @@
 
     <!-- Section: Links  -->
     <section class="">
-        <div class="container text-center text-md-start mt-5">
+        <div class="container-fluid text-center text-md-start">
             <!-- Grid row -->
-            <div class="row mt-3">
+            <div class="row text-center">
                 <!-- Grid column -->
-                <div class="col-lg-4 text-center col-xl-4 mx-auto mb-4">
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-4 mx-auto mb-4">
                     <!-- Content -->
                     <h6 class="text-uppercase fw-bold mb-4">
-                        <i class="fas fa-gem me-3"></i>MAIMT
+                        <i class="fas fa-gem me-3"></i>Company name
                     </h6>
                     <p>
-                        Approved by AICTE, Affiliated to Kurukshetra University, Kurukshetra, ISO 9001:2008 Certified
+                        Here you can use rows and columns to organize your footer content. Lorem ipsum
+                        dolor sit amet, consectetur adipisicing elit.
                     </p>
                 </div>
                 <!-- Grid column -->
 
                 <!-- Grid column -->
-                <div class="col-lg-4 col-xl-4 text-center mx-auto mb-4">
+                <div class="col-sm-6 col-md-2 col-lg-3 mt-4 mx-auto mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
-                        About MAIMT
+                        Products
                     </h6>
                     <p>
-                        <a href="#!" class="text-reset">About Us</a>
+                        <a href="#!" class="text-reset">Angular</a>
                     </p>
                     <p>
-                        <a href="#!" class="text-reset">Journal</a>
+                        <a href="#!" class="text-reset">React</a>
                     </p>
                     <p>
-                        <a href="#!" class="text-reset">Award Ranking</a>
+                        <a href="#!" class="text-reset">Vue</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Laravel</a>
                     </p>
                 </div>
                 <!-- Grid column -->
 
                 <!-- Grid column -->
-                <div class="col-lg-4 col-xl-4 text-center mx-auto mb-4">
+                <div class="col-sm-6 col-md-2 col-lg-3 mt-4 mx-auto mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
                         Useful links
                     </h6>
                     <p>
-                        <a href="#!" class="text-reset">AICTE</a>
+                        <a href="#!" class="text-reset">Pricing</a>
                     </p>
                     <p>
-                        <a href="#!" class="text-reset">KUK</a>
+                        <a href="#!" class="text-reset">Settings</a>
                     </p>
                     <p>
-                        <a href="#!" class="text-reset">UGC</a>
+                        <a href="#!" class="text-reset">Orders</a>
                     </p>
                     <p>
-                        <a href="#!" class="text-reset">Open Source</a>
+                        <a href="#!" class="text-reset">Help</a>
                     </p>
                 </div>
                 <!-- Grid column -->
 
                 <!-- Grid column -->
-                < <!-- Grid column -->
+                <div class="col-sm-6 col-md-4 col-lg-3 mt-4 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Contact
+                    </h6>
+                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        info@example.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
             </div>
             <!-- Grid row -->
         </div>
--- index.php
+++ index.php
@@ -8,14 +8,16 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 
     <title>Hello, world!</title>
 </head>
 
 <body>
-    <?php include 'Components/Header/Header.php' ?>
-    <?php include 'Components/Footer/Footer.php' ?>
-
+    <div class="container-fluid">
+        <?php include 'Components/Header/Header.php' ?>
+        <?php include 'Components/Footer/Footer.php' ?>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
 </body>
