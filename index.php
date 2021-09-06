84450d52f54ab2e42748d58dc20a1ab282bba41c
Avii <avii.official00@gmail.com>
2021-09-06 15:48:57 +0530

    Footer

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
 <body>
     <?php include 'Components/Header/Header.php' ?>
+    <?php include 'Components/Footer/Footer.php' ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
