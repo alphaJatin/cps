2e0cb8e4c306d7d1a4f7379afa5e3e0b89707802
Avii <avii.official00@gmail.com>
2021-09-07 12:24:50 +0530

    Hey

--- Components/Header/Header.php
+++ Components/Header/Header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light p-3">
   <div class="container-fluid">
     <!-- <a class="navbar-brand fs-2 px-3" href="#"><img src="Components/Header/images/logo.png" alt="MAIMT LOGO" width="50p" /></a> -->
-    <a class="navbar-brand fs-2 px-4" href="#">M A I M T</a>
+    <a class="navbar-brand fs-2 px-4" href="#"><img src="<?php echo $_SERVER['SERVER_NAME']; ?>" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
