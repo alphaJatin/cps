7c4e4d45cbca93248e9fbe10df3898f1a872e5dc
Avii <avii.official00@gmail.com>
2021-09-07 12:31:51 +0530

    Hey

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
 </head>
 
 <body>
-<?php include 'Components/Header/Header.php' ?>
+    <?php include 'Components/Header/Header.php' ?>
 
-<?php include 'Components/Carousel/Carousel.html' ?>
+    <?php include 'Components/Carousel/Carousel.html' ?>
 
-<?php include 'Components/Footer/Footer.php' ?>
+    <?php include 'Components/Footer/Footer.php' ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 
 </html>
