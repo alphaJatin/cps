9c179060a6c316d375268daa76d6d80ec23c3bbb
Avii <avii.official00@gmail.com>
2021-09-12 09:34:09 +0530

    Fix

--- delete.php
+++ delete.php
@@ -4,8 +4,10 @@ if ($_SESSION['logIn'] === true && $_SESSION['type'] === 'admin') {
     require_once "./config/db_con.php";
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
-        $q = "DELETE FROM student where id=$id;";
-        if (!$con->query($q)) echo $con->connect_error;
-        else exit(header("location: ./view.php"));
+        $q = "DELETE FROM student WHERE id=$id";
+        if (!$con->query($q)) {
+            echo "Hi";
+            echo $con->connect_error;
+        } else exit(header("location: ./view.php"));
     } else exit(header("location: ./login.php"));
-}
+} else exit(header("location: ./login.php"));
