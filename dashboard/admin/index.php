8845741268e4cd646638e670777fb768b6516281
Avii <avii.official00@gmail.com>
2021-09-13 14:13:00 +0530

    Fix

--- dashboard/admin/index.php
+++ dashboard/admin/index.php
@@ -53,7 +53,7 @@ if ($_SESSION['logIn'] === true && $_SESSION['type'] === 'admin') {
                             </div>
                             Applied Students
                         </a>
-                        <a class="nav-link" href="#">
+                        <a class="nav-link" href="../../view.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-user-graduate"></i>
                             </div>
                             View Students
